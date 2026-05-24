09e3222890a7012583c0fcd74b653f2aa4adf92f
Sree-1211 <sreeshma1211@gmail.com>
2026-05-24 18:57:06 +0530

    Added Raylib C# practice project

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using Raylib_cs;
+
+namespace TestProject_;
+class Program
+{
+    private static float pos=10;
+    public static void Main()
+    {
+        Raylib.InitWindow(500, 500, "test window");
+        while (!Raylib.WindowShouldClose())
+        {
+            Raylib.BeginDrawing();
+            Draw();
+            Raylib.EndDrawing();
+        }
+        Raylib.CloseWindow();
+    }
+    private static void Draw()
+    {
+        pos+=0.01f;
+        Raylib.DrawText("Hello", 10, (int)pos, 20, Color.White);
+        Raylib.ClearBackground(Color.DarkGray);
+    }
+}
